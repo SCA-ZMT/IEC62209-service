3c64d2a921db4cf0adcd6677b470928d998058a8
Simone Callegari <callegari@zmt.swiss>
2023-04-26 11:08:56 +0200

    more work

---
+++ server/src/iec62209_service/routers/training_set_generation.py
@@ -0,0 +1,60 @@
+from os.path import dirname, realpath
+
+from fastapi import APIRouter, status
+from fastapi.responses import FileResponse, JSONResponse, PlainTextResponse
+
+from .common import SampleConfig, SampleInterface, SarFiltering
+
+router = APIRouter(
+    prefix="/training-set-generation",
+    tags=["training-set-generation"],
+    responses={404: {"error": "Backend not ready"}},
+)
+
+# Training set generation
+
+
+@router.get("/reset")
+async def training_set_reset():
+    SampleInterface.trainingSet.clear()
+
+
+@router.get("/distribution", response_class=FileResponse)
+async def training_set_distribution() -> FileResponse:
+    return FileResponse(
+        dirname(realpath(__file__)) + "/../../../../assets/mwl.png",
+        media_type="image/png",
+    )
+
+
+@router.get("/data", response_class=JSONResponse)
+async def training_set_data() -> JSONResponse:
+    return SampleInterface.trainingSet
+
+
+@router.get("/xport", response_class=PlainTextResponse)
+async def training_set_xport() -> PlainTextResponse:
+    need_extra_colums = False
+    headings = SampleInterface.trainingSet.headings
+    if "sar_1g" not in headings:
+        need_extra_colums = True
+        headings += [SarFiltering.SAR10G.lower(), "u10g"]
+    text = str(SampleInterface.trainingSet.headings).strip("[]")
+    for row in SampleInterface.trainingSet.rows:
+        if need_extra_colums:
+            row += [0, 0]
+        text += "\n" + str(row).strip("[]")
+    return PlainTextResponse(text)
+
+
+@router.post("/generate", response_class=JSONResponse)
+async def training_set_generate(config: SampleConfig) -> JSONResponse:
+    message = ""
+    end_status = status.HTTP_200_OK
+    try:
+        SampleInterface.trainingSet.generate(config)
+    except Exception as e:
+        message = f"The IEC62209 package raised an exception: {e}"
+        end_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    return JSONResponse(message, status_code=end_status)
