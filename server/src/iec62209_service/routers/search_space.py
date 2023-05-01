4c6ce775543565aa13290bde4d1ac2d700d9ec17
Simone Callegari <callegari@zmt.swiss>
2023-05-01 17:15:50 +0200

    added /test-set-generation/model-area and /search-space/model-area GET

--- server/src/iec62209_service/routers/search_space.py
+++ server/src/iec62209_service/routers/search_space.py
@@ -35,3 +35,18 @@ async def critical_set_xport() -> FileResponse:
     tmp = NamedTemporaryFile(delete=False)
     SampleInterface.criticalSet.export_to_csv(tmp.name)
     return FileResponse(tmp.name, media_type="text/csv")
+
+
+@router.get("/model-area", response_class=JSONResponse)
+async def critical_set_get_model_area() -> JSONResponse:
+    ModelInterface.raise_if_no_model()
+    conf = SampleInterface.testSet.config
+    if conf.sampleSize > 0:
+        return {
+            "measAreaX": f"{conf.measAreaX:.0f}",
+            "measAreaY": f"{conf.measAreaY:.0f}",
+        }
+    return JSONResponse(
+        {"error": "Sample not loaded from model"},
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
