cd7bbc01ea29e5e386afe28ab6ea1e1f2804e16b
Simone Callegari <callegari@zmt.swiss>
2023-05-10 14:21:32 +0200

    forgot script

---
+++ update_version.sh
@@ -0,0 +1,5 @@
+CUR_VERSION=1.0.89
+
+sed 's/^version: .*/version: '$CUR_VERSION'/g' .osparc/iec62209-web/metadata.yml.in > .osparc/iec62209-web/metadata.yml
+sed 's/^    version=.*,/    version=\"'$CUR_VERSION'\",/g' server/setup.py.in > server/setup.py
+sed 's/  \"version\":.*/  \"version\": \"'$CUR_VERSION'\",/g' client/package.json.in > client/package.json
