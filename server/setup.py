ad281505a81286d0477f9f8b8a2255c953a5c7b8
Simone Callegari <callegari@zmt.swiss>
2023-05-11 13:48:07 +0200

    fix for empty deviations plot

--- server/setup.py
+++ server/setup.py
@@ -23,7 +23,7 @@ INSTALL_REQUIREMENTS = list(read_reqs(CURRENT_DIR / "requirements.txt"))
 
 SETUP = dict(
     name="iec62209-service",
-    version="0.0.0",
+    version="1.0.17",
     description="Service built around publication-IEC62209",
     author=", ".join(
         (
--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
 CUR_VERSION=`awk '{ print $1; }' VERSION`
 
-sed -i '/^version: .*/version: '$CUR_VERSION'/' .osparc/iec62209-web/metadata.yml
-sed -i '/^    version=.*,/    version=\"'$CUR_VERSION'\",/' server/setup.py
-sed -i '/  \"version\":.*/  \"version\": \"'$CUR_VERSION'\",/' client/package.json
+sed -i '/^version: .*/c\version: '$CUR_VERSION .osparc/iec62209-web/metadata.yml
+sed -i '/^    version=.*,/c\    version=\"'$CUR_VERSION'\",' server/setup.py
+sed -i '/  \"version\":.*/c\  \"version\": \"'$CUR_VERSION'\",' client/package.json
